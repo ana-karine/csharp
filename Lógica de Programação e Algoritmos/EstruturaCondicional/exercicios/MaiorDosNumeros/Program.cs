aea9a581ed3b3e22d7794b94c49f71f3c29ae89b
ana-karine <anakarine.ti@gmail.com>
2023-06-09 01:34:40 -0300

    exercicio MaiorDosNumeros

---
+++ Lógica de Programação e Algoritmos/EstruturaCondicional/exercicios/MaiorDosNumeros/Program.cs
@@ -0,0 +1,20 @@
+﻿int numX, numY, numZ;
+string numbersString;
+string[] numbersVector; 
+
+Console.WriteLine("Escreva 3 números inteiros: ");
+numbersString = Console.ReadLine();
+
+numbersVector = numbersString.Split(' ');
+
+numX = int.Parse(numbersVector[0]);
+numY = int.Parse(numbersVector[1]);
+numZ = int.Parse(numbersVector[2]);
+
+if (numX > numY && numX > numZ) {
+    Console.WriteLine("O maior dos números é o " + numX);
+} else if (numY > numX && numY > numZ) {
+    Console.WriteLine("O maior dos números é o " + numY);
+} else {
+    Console.WriteLine("O maior dos números é o " + numZ);
+}
