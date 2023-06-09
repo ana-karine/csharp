a974c6bd55daa201ab1af0a5a96a3a712c15047c
ana-karine <anakarine.ti@gmail.com>
2023-06-09 10:12:01 -0300

    exercicios while

---
+++ Lógica de Programação e Algoritmos/EstruturaRepetitiva/While/Quadrante/Program.cs
@@ -0,0 +1,38 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1115
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string quadrante = "";
+        
+        string[] numbers = Console.ReadLine().Split(' ');
+
+        int X = int.Parse(numbers[0]);
+        int Y = int.Parse(numbers[1]);
+
+        while (X != 0 && Y != 0) {
+
+            if (X > 0 && Y > 0) {
+                quadrante = "primeiro";
+            } else if (X < 0 && Y > 0) {
+                quadrante = "segundo";
+            } else if (X < 0 && Y < 0) {
+                quadrante = "terceiro";
+            } else if (X > 0 && Y < 0) {
+                quadrante = "quarto";
+            }
+
+            Console.WriteLine(quadrante);
+
+            numbers = Console.ReadLine().Split(' ');
+
+            X = int.Parse(numbers[0]);
+            Y = int.Parse(numbers[1]);
+        }
+
+    }
+
+}
---
+++ Lógica de Programação e Algoritmos/EstruturaRepetitiva/While/SomaDeParesConsecutivos/Program.cs
@@ -0,0 +1,30 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1159
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int x = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        while (x != 0) {
+
+            if (x % 2 == 0) {
+                sum = x + (x + 2) + (x + 4) + (x + 6) + (x + 8);
+                sum = (5 * x) + 20;
+            } else {
+                sum = (x + 1) + (x + 1 + 2) + (x + 1 + 4) + (x + 1 + 6) + (x + 1 + 8);
+                sum = (5 * x) + 25;
+            }
+
+            Console.WriteLine(sum);
+
+            x = int.Parse(Console.ReadLine());
+            sum = 0;
+       }
+
+    }
+
+}
