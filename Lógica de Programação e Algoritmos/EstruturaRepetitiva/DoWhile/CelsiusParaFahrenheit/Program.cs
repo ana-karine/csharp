6f17d6ecfcaf8e904af9d717fb2e7f7a2266e9ba
ana-karine <anakarine.ti@gmail.com>
2023-06-09 11:48:57 -0300

    exercicio do while

---
+++ Lógica de Programação e Algoritmos/EstruturaRepetitiva/DoWhile/CelsiusParaFahrenheit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+char repetir;
+double C = 0.0;
+double F = 0.0;
+
+do {
+    Console.WriteLine("Digite a temperatura em Celsius: ");
+    C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    F = 1.8 * C + 32;
+
+    Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("F1", CultureInfo.InvariantCulture));
+
+    Console.WriteLine("Deseja repetir (s/n)?");
+    repetir = char.Parse(Console.ReadLine());
+} while (repetir == 's');
