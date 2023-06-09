5d3964882cbc75d666f6d3c6115e6aa520738b82
ana-karine <anakarine.ti@gmail.com>
2023-06-09 14:49:55 -0300

    exercicios de estrutura repetitiva

---
+++ Lógica de Programação e Algoritmos/EstruturaRepetitiva/exercicios/MenorNumeroEPosicao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace URI {
+    class Program {
+        static void Main(string[] args) {
+
+            // vamos considerar o primeiro valor como sendo inicialmente o menor
+            int menor = int.Parse(Console.ReadLine());
+            int posicao = 1;
+
+            // agora vamos ler o segundo até o centésimo 
+            for (int i = 2; i <= 100; i++) {
+                int x = int.Parse(Console.ReadLine());
+                if (x < menor) {
+                    menor = x;
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine(menor);
+            Console.WriteLine(posicao);
+        }
+    }
+}
