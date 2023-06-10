417c73de26d61e7b048356e5afe20ef714bcd562
ana-karine <anakarine.ti@gmail.com>
2023-06-10 13:37:18 -0300

    exemplo básico de matrizes

---
+++ Lógica de Programação e Algoritmos/Matrizes/ExemploBasico/Program.cs
@@ -0,0 +1,25 @@
+﻿// http://www.linhadecodigo.com.br/artigo/3444/arrays-em-csharp-teoria-e-pratica.aspx
+
+// Exemplo: utilizar uma matriz para montar uma “cartela de bingo”, onde as casas são informadas pelo usuário.
+
+int[,] bingo = new int[2,2];
+
+for (int l = 0; l < 2; l++) 
+{
+    for (int c = 0; c < 2; c++)
+    {
+        Console.WriteLine(String.Format("Informe o elemento ({0},{1})", l, c));
+        bingo[l,c] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+
+for (int l = 0; l < 2; l++) 
+{
+    for (int c = 0; c < 2; c++)
+    {
+        Console.Write(bingo[l,c] + " ");
+    }
+    Console.WriteLine();
+}
