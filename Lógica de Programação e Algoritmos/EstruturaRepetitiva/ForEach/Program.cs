ac60a60e229f49e76e439000cbabfa25b09c8aa4
ana-karine <anakarine.ti@gmail.com>
2023-06-11 04:03:59 -0300

    exemplo basico foreach

---
+++ Lógica de Programação e Algoritmos/EstruturaRepetitiva/ForEach/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Informe o tamanho do vetor: ");
+int N = int.Parse(Console.ReadLine());
+
+string[] nomes = new string[N];
+
+Console.WriteLine("\nInforme os " + N + " nomes: ");
+for (int i = 0; i < N; i++) {
+    nomes[i] = Console.ReadLine();
+}
+
+/*Console.WriteLine("\Nomes lidos: ");
+for (int i = 0; i < N; i++) {
+    Console.WriteLine(nomes[i]);
+}*/
+
+Console.WriteLine("\nNomes lidos: ");
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
